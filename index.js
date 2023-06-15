75e973a9a82b12aa25ae375d657e4d032c641e13
Volodymyr Machula <machulav@gmail.com>
2023-06-15 22:43:34 +0200

    Update

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports = {
         verifyDomain,
         checkIfDomainIsVerified,
         //checkIfDnsIsProperlyConfigured,
-        //retryDomainVerificationOnAWS,
         //addEmailToDatabase
     ],
     configurationParameters: [
